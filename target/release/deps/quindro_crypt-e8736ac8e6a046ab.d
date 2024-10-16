3102c37305d107582cf5da93d6709b8b32ca3560
Kopernikus73 <kopernikus3.141@gmail.com>
2024-10-16 12:47:58 +0200

    1.0.1

--- target/release/deps/quindro_crypt-e8736ac8e6a046ab.d
+++
@@ -1,5 +0,0 @@
-/home/kopernikus/Eugen/Rust/quindro-crypt/target/release/deps/quindro_crypt-e8736ac8e6a046ab: src/main.rs
-
-/home/kopernikus/Eugen/Rust/quindro-crypt/target/release/deps/quindro_crypt-e8736ac8e6a046ab.d: src/main.rs
-
-src/main.rs:
--- target/release/quindro-crypt.d
+++
@@ -1,1 +0,0 @@
-/home/kopernikus/Eugen/Rust/quindro-crypt/target/release/quindro-crypt: /home/kopernikus/Eugen/Rust/quindro-crypt/src/main.rs
